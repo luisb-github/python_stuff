1a8f2da559aec9c27e1c211b4efb0352ae102d27
lulub <lulub.esteves@gmail.com>
2023-04-04 18:24:12 +0100

    ui shortcuts list and actions + action menu toggle

--- Fleet/organizador2.py
+++ Fleet/organizador2.py
@@ -13,9 +13,8 @@ class ShortcutsView(ft.UserControl):
             ]
         )
 
-        self.shortcuts_list = ft.Column(expand=True, spacing=14)
-        self.shortcuts_list.controls.append(ft.Text("self.title", expand=True))
-        # self.load_shortcuts_to_list()
+        self.shortcuts_list = ft.ListView(spacing=14, auto_scroll=True)
+        self.load_shortcuts_to_list()
 
         return ft.Column(
             controls=[
@@ -41,10 +40,12 @@ class ShortcutsView(ft.UserControl):
             shortcut = Shortcut(index, content["title"], content["path"])
             self.shortcuts_list.controls.append(shortcut)
         # add shortcut to array
-        self.update
 
 
 class Shortcut(ft.UserControl):
+
+    actions_menu_active = False
+
     def __init__(self, id, title, path):
         super().__init__()
         self.id = id
@@ -52,7 +53,52 @@ class Shortcut(ft.UserControl):
         self.path = path
 
     def build(self):
-        return ft.Text(self.title, expand=True)
+        self.info_display = ft.Row(
+            alignment=ft.MainAxisAlignment.START,
+            spacing=20,
+            controls=[
+                ft.Text(self.id, style=ft.TextThemeStyle.LABEL_LARGE,
+                        color="White",),
+                ft.Text(self.title, style=ft.TextThemeStyle.TITLE_MEDIUM,
+                        color="White", expand=True),
+            ]
+        )
+
+        self.action_list = ft.ResponsiveRow(
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN, expand=True)
+
+        self.load_actions_to_list()
+
+        self.actions = ft.Container(
+            visible=self.actions_menu_active,
+            border_radius=24,
+            padding=16,
+            bgcolor=ft.colors.BLACK12,
+            content=self.action_list
+        )
+
+        return ft.Card(
+            expand=True,
+            content=ft.Container(
+                padding=ft.padding.symmetric(vertical=20, horizontal=20),
+                border_radius=12,
+                on_click=self.actions_menu_toggle,
+                content=ft.Column(
+                    expand=True,
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    spacing=22,
+                    controls=[
+                        self.info_display,
+                        self.actions
+                    ]
+                )
+            )
+        )
+
+    def actions_menu_toggle(self, e):
+        self.actions.visible = not self.actions_menu_active
+        self.actions_menu_active = not self.actions_menu_active
+        self.update()
 
     def edit(self, e):
         pass
@@ -60,13 +106,40 @@ class Shortcut(ft.UserControl):
     def delete(self, e):
         pass
 
+    def read_stored_action(self):
+        actions = [
+            {"action": "open", "hotkey": "o"},
+            {"action": "change to", "hotkey": "c"},
+            {"action": "move to", "hotkey": "m"},
+            {"action": "edit", "hotkey": "e", "type": "edition"},
+            {"action": "delete", "hotkey": "d", "type": "edition"},
+        ]
+
+        return actions
+
+    def load_actions_to_list(self):
+        actions = self.read_stored_action()
+
+        for action in actions:
+            action_title = f'{action["action"]} ({action["hotkey"]})'.capitalize(
+            )
+
+            if action.get('type'):
+                action_button = ft.OutlinedButton(action_title, col={"xs": 6, "sm": 6, "md": 2}, on_click=lambda e: print(
+                    "action clicked!"),)
+            else:
+                action_button = ft.FilledTonalButton(action_title, col={"xs": 12, "sm": 6, "md": 2}, on_click=lambda e: print(
+                    "action clicked!"),)
+
+            self.action_list.controls.append(action_button)
+
 
 def main(page: ft.Page):
 
     # window settings
     window_position_x = 2100
     window_position_y = 700
-    window_width = 350
+    window_width = 360
     window_height = 700
 
     # page.window_left = window_position_x
