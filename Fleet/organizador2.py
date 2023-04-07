bc7f27766e644c9cbcf6c944c363769f16f34f2e
lulub <lulub.esteves@gmail.com>
2023-04-07 03:07:23 +0100

    edit working, read action clicks, start edit alert

--- Fleet/organizador2.py
+++ Fleet/organizador2.py
@@ -1,6 +1,8 @@
 import flet as ft
+import tkinter as tk
+from tkinter import filedialog
 
-get_directory_dialog = ""
+main_page = ft.page
 
 
 class ShortcutsView(ft.UserControl):
@@ -64,6 +66,7 @@ class ShortcutsView(ft.UserControl):
 class Shortcut(ft.UserControl):
 
     actions_menu_active = False
+    edit_mode = False
 
     def __init__(self, id, title, path):
         super().__init__()
@@ -72,28 +75,46 @@ class Shortcut(ft.UserControl):
         self.path = path
 
     def build(self):
+        self.info_txt_id = ft.Text(self.id, style=ft.TextThemeStyle.LABEL_LARGE,
+                                   color="White",)
+        self.info_txt_title = ft.Text(self.title, style=ft.TextThemeStyle.TITLE_MEDIUM,
+                                      color="White", expand=True)
         self.info_display = ft.Row(
             alignment=ft.MainAxisAlignment.START,
             spacing=20,
             controls=[
-                ft.Text(self.id, style=ft.TextThemeStyle.LABEL_LARGE,
-                        color="White",),
-                ft.Text(self.title, style=ft.TextThemeStyle.TITLE_MEDIUM,
-                        color="White", expand=True),
+                self.info_txt_id,
+                self.info_txt_title
             ]
         )
 
-        self.info_display.visible = False
+        # get_directory_dialog.current.on_result = self.get_location_path
+        self.txt_field_id = ft.TextField(label="ID", value=self.id, border=ft.InputBorder.UNDERLINE, keyboard_type=ft.KeyboardType.NUMBER,
+                                         max_length=1, col=3, text_align=ft.TextAlign.CENTER, dense=True)
+        self.txt_field_title = ft.TextField(label="Title", value=self.title, border=ft.InputBorder.UNDERLINE,
+                                            max_length=12, border_radius=24, col=8, dense=True)
+        self.txt_path = ft.Text(
+            str(self.path) if self.path else "no location", col=9)
+
+        self.get_directory_dialog = ft.FilePicker(
+            on_result=self.get_location_path)
+
+        global main_page
+        main_page.overlay.extend([self.get_directory_dialog])
 
         self.edit_shortcut_mode = ft.ResponsiveRow(
+            visible=self.edit_mode,
             alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
             controls=[
-                ft.TextField(label="ID", border="underline", keyboard_type=ft.KeyboardType.NUMBER,
-                             max_length=1, col=3, text_align=ft.TextAlign.CENTER, dense=True),
-                ft.TextField(label="Title", border="underline",
-                             max_length=8, border_radius=24, col=8, dense=True),
-                ft.FilledButton("Location", icon=ft.icons.FOLDER_OPEN,
-                                on_click=lambda _: get_directory_dialog.get_directory_path(),)
+                self.txt_field_id,
+                self.txt_field_title,
+                ft.IconButton(icon=ft.icons.FOLDER_OPEN,
+                              on_click=self.get_directory_dialog.get_directory_path, col=3),
+                self.txt_path,
+                ft.TextButton("Cancel", col=5,
+                              on_click=self.cancel),
+                ft.FilledButton("Save", icon=ft.icons.SAVE, col=5,
+                                on_click=self.save),
             ]
         )
 
@@ -109,6 +130,11 @@ class Shortcut(ft.UserControl):
             bgcolor=ft.colors.BLACK12,
             content=self.action_list
         )
+
+        self.alert_dialog = ft.AlertDialog(
+            title=ft.Text("Acabar o alearta de erro e o select folder demasiado lento"))
+        main_page.dialog = self.alert_dialog
+        self.alert_dialog.open = True
 
         return ft.Card(
             expand=True,
@@ -130,12 +156,73 @@ class Shortcut(ft.UserControl):
         )
 
     def actions_menu_toggle(self, e):
-        self.actions.visible = not self.actions_menu_active
-        self.actions_menu_active = not self.actions_menu_active
+        if not self.edit_mode:
+            self.actions.visible = not self.actions_menu_active
+            self.actions_menu_active = not self.actions_menu_active
+            self.update()
+
+    def edit(self):
+        # make info and actions disappear
+        self.info_display.visible = False
+        self.actions.visible = False
+
+        # enable edit mode
+        self.edit_mode = True
+        self.edit_shortcut_mode.visible = True
+
         self.update()
 
-    def edit(self, e):
-        pass
+    def save(self, e):
+        txt_field_id = str(self.txt_field_id.value)
+        txt_field_title = self.txt_field_title.value
+        txt_path = self.txt_path.value
+
+        if txt_field_id.isdigit() and txt_field_id and txt_field_title and txt_path:
+
+            self.id = txt_field_id
+            self.title = txt_field_title
+            self.path = txt_path
+
+            self.info_txt_id.value = self.id
+            self.info_txt_title.value = self.title
+
+            # disable edit mode
+            self.edit_mode = False
+            self.edit_shortcut_mode.visible = False
+
+            # make info and actions disappear
+            self.info_display.visible = True
+            self.actions.visible = True
+
+            self.update()
+
+        else:
+            if not txt_field_id.isdigit():
+                self.alert_dialog.title = ft.Text(
+                    "ID must be numeric between 1-6")
+                self.alert_dialog.open = True
+            elif not (txt_field_id and txt_field_title and txt_path):
+                self.alert_dialog.title = ft.Text("Fill empty camps")
+                self.alert_dialog.open = True
+
+    def cancel(self, e):
+        self.txt_path.value = self.path
+
+        # disable edit mode
+        self.edit_mode = False
+        self.edit_shortcut_mode.visible = False
+
+        # make info and actions disappear
+        self.info_display.visible = True
+        self.actions.visible = True
+
+        self.update()
+
+    def get_location_path(self, e):
+        if e.path:
+            path = e.path
+            self.txt_path.value = path
+            self.update()
 
     def delete(self, e):
         pass
@@ -152,29 +239,41 @@ class Shortcut(ft.UserControl):
         return actions
 
     def load_actions_to_list(self):
+        # get actions
         actions = self.read_stored_action()
 
+        # add actions to list
         for action in actions:
+
+            # create a string where it joins the name of the action and their hotkey
             action_title = f'{action["action"]} ({action["hotkey"]})'.capitalize(
             )
 
+            # make actions that affect the shortcuts visually diferent from real actions
             if action.get('type'):
-                action_button = ft.OutlinedButton(action_title, col={"xs": 6, "sm": 6, "md": 2}, on_click=lambda e: print(
-                    "action clicked!"),)
+                action_button = ft.OutlinedButton(
+                    action_title, col={"xs": 6, "sm": 6, "md": 2}, on_click=self.read_action,)
             else:
-                action_button = ft.FilledTonalButton(action_title, col={"xs": 12, "sm": 6, "md": 2}, on_click=lambda e: print(
-                    "action clicked!"),)
+                action_button = ft.FilledTonalButton(
+                    action_title, col={"xs": 12, "sm": 6, "md": 2}, on_click=self.read_action,)
 
             self.action_list.controls.append(action_button)
 
+    def read_action(self, e):
+        # gets the hotkey (mouse clicked)
+        hotkey = e.control.text[-2:-1]
 
-# Open directory dialog
-def get_directory_result(e: ft.FilePickerResultEvent):
-    path = e.path if e.path else "Cancelled!"
-    print(path)
+        match hotkey:
+            case "e":
+                self.edit()
+            case _:
+                print("not valid hotkey")
 
 
 def main(page: ft.Page):
+
+    global main_page
+    main_page = page
 
     # window settings
     window_position_x = 2100
@@ -193,12 +292,6 @@ def main(page: ft.Page):
     page.window_always_on_top = True
 
     page.update()
-
-    global get_directory_dialog
-    get_directory_dialog = ft.FilePicker(
-        on_result=get_directory_result)
-
-    page.overlay.extend([get_directory_dialog])
 
     # create application instance
     shortcut_view = ShortcutsView()
